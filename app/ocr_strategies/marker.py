add5c43d58ebabfe1937c07d9b8d17683d0b250d
Adrian Chojnicki <adrian@chojnicki.pl>
2025-01-07 20:04:27 +0100

    + wip to squash

--- app/ocr_strategies/marker.py
+++ app/ocr_strategies/marker.py
@@ -1,12 +1,13 @@
 from marker.convert import convert_single_pdf
+from marker.convert import convert_single_pdf
 from marker.models import load_all_models
 
 from ocr_strategies.ocr_strategy import OCRStrategy
 
 class MarkerOCRStrategy(OCRStrategy):
     """Marker OCR Strategy"""
-    def extract_text_from_pdf(self, pdf_bytes):
+    def extract_text_from_pdf(self, file_bytes):
         model_lst = load_all_models()
-        full_text, images, out_meta = convert_single_pdf(pdf_bytes, model_lst)
+        full_text, images, out_meta = convert_single_pdf(file_bytes, model_lst)
         return full_text
 
