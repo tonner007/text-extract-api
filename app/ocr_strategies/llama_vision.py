7ce29f3ac48e8f6d3fb56a60d919f085d19f91ae
Adrian Chojnicki <adrian@chojnicki.pl>
2025-01-09 07:37:55 +0100

    # 59 merge with repo - need to check differences between original and current

--- app/ocr_strategies/llama_vision.py
+++ app/ocr_strategies/llama_vision.py
@@ -8,7 +8,6 @@ import base64
 import tempfile
 from pdf2image import convert_from_bytes
 
-
 class LlamaVisionOCRStrategy(OCRStrategy):
     """Llama 3.2 Vision OCR Strategy"""
 
@@ -23,7 +22,7 @@ class LlamaVisionOCRStrategy(OCRStrategy):
             # Convert image to base64
             buffered = io.BytesIO()
             image.save(buffered, format="JPEG")
-            #img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            # img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
             # Save image to a temporary file and get its path
             temp_filename = None
             with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_file:
@@ -34,17 +33,23 @@ class LlamaVisionOCRStrategy(OCRStrategy):
             try:
                 response = ollama.chat("llama3.2-vision", [{
                     'role': 'user',
-                    'content':  os.getenv('LLAMA_VISION_PROMPT', "You are OCR. Convert image to markdown."),
+                    'content': os.getenv('LLAMA_VISION_PROMPT', "You are OCR. Convert image to markdown."),
                     'images': [temp_filename]
                 }], stream=True)
                 os.remove(temp_filename)
                 num_chunk = 1
                 for chunk in response:
-                    self.update_state_callback(state='PROGRESS', meta={'progress': str(30 + ocr_percent_done), 'status': 'OCR Processing (page ' + str(i+1) + ' of ' + str(num_pages) +') chunk no: ' + str(num_chunk), 'start_time': start_time, 'elapsed_time': time.time() - start_time})  # Example progress update
+                    self.update_state_callback(state='PROGRESS', meta={'progress': str(30 + ocr_percent_done),
+                                                                       'status': 'OCR Processing (page ' + str(
+                                                                           i + 1) + ' of ' + str(
+                                                                           num_pages) + ') chunk no: ' + str(num_chunk),
+                                                                       'start_time': start_time,
+                                                                       'elapsed_time': time.time() - start_time})  # Example progress update
                     num_chunk += 1
                     extracted_text += chunk['message']['content']
 
-                ocr_percent_done += int(20/num_pages) #20% of work is for OCR - just a stupid assumption from tasks.py
+                ocr_percent_done += int(
+                    20 / num_pages)  # 20% of work is for OCR - just a stupid assumption from tasks.py
             except ollama.ResponseError as e:
                 print('Error:', e.error)
                 raise Exception("Failed to generate text with Llama 3.2 Vision model")
