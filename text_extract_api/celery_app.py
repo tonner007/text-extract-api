aaf24d8150efeac53ae4e44a2a8a71df52b340a6
Adrian Chojnicki <adrian@chojnicki.pl>
2025-01-12 16:44:43 +0100

    + fix lost history

---
+++ text_extract_api/celery_app.py
@@ -0,0 +1,28 @@
+import pathlib
+import sys
+
+from celery import Celery
+from dotenv import load_dotenv
+
+from text_extract_api.extract.ocr_strategies.ocr_strategy import OCRStrategy
+
+sys.path.insert(0, str(pathlib.Path(__file__).parent.resolve()))
+
+load_dotenv(".env")
+
+import multiprocessing
+
+multiprocessing.set_start_method("spawn", force=True)
+
+app = Celery(
+    "text_extract_api",
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0"
+)
+app.config_from_object({
+    "worker_max_memory_per_child": 8200000
+})
+
+
+OCRStrategy.autodiscover_strategies()
+app.autodiscover_tasks(["text_extract_api.extract"], 'tasks', True)
---
+++ text_extract_api/extract/ocr_strategies/llama_vision.py
@@ -0,0 +1,65 @@
+import os
+import tempfile
+import time
+from typing import Type
+
+import ollama
+
+from text_extract_api.extract.ocr_strategies.ocr_strategy import OCRStrategy
+from text_extract_api.files.file_formats.file_format import FileFormat
+from text_extract_api.files.file_formats.image_file_format import ImageFileFormat
+
+
+class LlamaVisionOCRStrategy(OCRStrategy):
+    """Llama 3.2 Vision OCR Strategy"""
+
+    @classmethod
+    def name(cls) -> str:
+        return "llama_vision"
+
+    def extract_text(self, file_format: FileFormat):
+        if not file_format.can_convert_to(ImageFileFormat):
+            raise Exception(f"Llama Vision does not handle files of mime type: {file_format.mime_type}")
+
+        images = FileFormat.convert_to(file_format, ImageFileFormat);
+        extracted_text = ""
+        start_time = time.time()
+        ocr_percent_done = 0
+        num_pages = len(images)
+        for i, image in enumerate(images):
+
+            with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_file:
+                temp_file.write(image.binary)
+                temp_filename = temp_file.name
+
+            # Generate text using the Llama 3.2 Vision model
+            try:
+                print(temp_filename)
+                response = ollama.chat("llama3.2-vision", [{
+                    'role': 'user',
+                    'content':  os.getenv('LLAMA_VISION_PROMPT', "You are OCR. Convert image to markdown."),
+                    'images': [temp_filename]
+                }], stream=True)
+                os.remove(temp_filename)
+                num_chunk = 1
+                for chunk in response:
+                    meta = {
+                        'progress': str(30 + ocr_percent_done),
+                        'status': 'OCR Processing'
+                                  + '(page ' + str(i + 1) + ' of ' + str(num_pages) + ')'
+                                  + ' chunk no: ' + str(num_chunk),
+                        'start_time': start_time,
+                        'elapsed_time': time.time() - start_time}
+                    self.update_state_callback(state='PROGRESS', meta=meta)
+                    num_chunk += 1
+                    extracted_text += chunk['message']['content']
+
+                ocr_percent_done += int(
+                    20 / num_pages)  # 20% of work is for OCR - just a stupid assumption from tasks.py
+            except ollama.ResponseError as e:
+                print('Error:', e.error)
+                raise Exception("Failed to generate text with Llama 3.2 Vision model")
+
+            print(response)
+
+        return extracted_text
