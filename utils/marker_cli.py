465ffb10cda65080d408d7220af5945a3a50f100
pkarw <piotr.karwatka@gmail.com>
2025-01-13 14:51:25 +0100

    [fix] surya requirement reversed + path fix

--- utils/marker_cli.py
+++ utils/marker_cli.py
@@ -4,7 +4,7 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process a PDF/Office/Image file.")
-    parser.add_argument("file", type=str, nargs='?', default="../examples/example-mri.pdf", help="The path to the PDF/Office/Image file to be processed.")
+    parser.add_argument("file", type=str, nargs='?', default="./examples/example-mri.pdf", help="The path to the PDF/Office/Image file to be processed.")
     args = parser.parse_args()
 
     model_lst = load_all_models()
