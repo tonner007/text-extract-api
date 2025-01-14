375cf667498d13613848ea5b736e1c1a15031384
Adrian Chojnicki <adrian@chojnicki.pl>
2025-01-14 07:53:18 +0100

    #85 + docker must have separated .venv from local because of symlinks

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,18 +3,20 @@
 PYPROJECT_HASH_FILE=".pyproject.hash"
 CURRENT_HASH=$(sha256sum pyproject.toml | awk '{ print $1 }')
 
-if [ ! -d ".venv" ] || [ ! -f "$PYPROJECT_HASH_FILE" ] || [ "$(cat $PYPROJECT_HASH_FILE)" != "$CURRENT_HASH" ]; then
-  echo "Dependencies have changed or .venv is missing. Reinstalling..."
-  python -m venv .venv
-  source .venv/bin/activate
+
+if [ ! -d ".dvenv" ] || [ ! -f "$PYPROJECT_HASH_FILE" ] || [ "$(cat $PYPROJECT_HASH_FILE)" != "$CURRENT_HASH" ]; then
+  echo "Dependencies have changed or .dvenv is missing. Reinstalling..."
+  python -m venv .dvenv
+  source .dvenv/bin/activate
   pip install --upgrade pip setuptools wheel
   pip install .
   echo "$CURRENT_HASH" > "$PYPROJECT_HASH_FILE"
 else
+  python3 -m venv --upgrade /app/.dvenv # temporary :(
   echo "Virtual environment is up to date."
 fi
 
-source .venv/bin/activate
+source .dvenv/bin/activate
 
 if [ "$APP_MODE" = "celery" ]; then
   echo "Starting Celery worker..."
